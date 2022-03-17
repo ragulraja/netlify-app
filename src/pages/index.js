000f4cbade6824df152eba9f84fba35a26343e10
Ragulraja <vragul0602@gmail.com>
2022-03-17 10:31:37 +0530

    hasura-setup

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,9 +5,16 @@ import Header from "../components/Header/Header";
 import Container from "@components/Container";
 import styles from "../styles/Home.module.scss";
 import Button from "@components/Button";
-import products from "../data/products.json";
 
-export default function Home() {
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  useQuery,
+  gql,
+} from "@apollo/client";
+
+export default function Home({ productsdata }) {
   return (
     <div>
       <Head>
@@ -26,34 +33,32 @@ export default function Home() {
         <Container>
           <h1>Related Products</h1>
           <ul className={styles.products}>
-            {products.map((product) => {
-              return (
-                // eslint-disable-next-line react/jsx-key
-                <li key={product.id}>
-                  <Image
-                    src={product.Image}
-                    alt={`Card of ${product.title}`}
-                    width={400}
-                    height={270}
-                  />
-                  <h3>{product.title}</h3>
-                  <p>${product.Price}</p>
-                  <p>
-                    <button
-                      className="snipcart-add-item"
-                      data-item-id={product.id}
-                      data-item-price={product.Price}
-                      data-item-url="/"
-                      data-item-description=""
-                      data-item-image={product.Image}
-                      data-item-name={product.title}
-                    >
-                      Add to Cart
-                    </button>
-                  </p>
-                </li>
-              );
-            })}
+            {productsdata.map((product) => (
+              // eslint-disable-next-line react/jsx-key
+              <li key={product.id}>
+                <Image
+                  src={`/images/${product.image}.jpg`}
+                  alt={`Card of ${product.title}`}
+                  width={600}
+                  height={600}
+                />
+                <h3>{product.title}</h3>
+                <p>${product.price}</p>
+                <p>
+                  <button
+                    className="snipcart-add-item"
+                    data-item-id={product.id}
+                    data-item-price={product.price}
+                    data-item-url="/"
+                    data-item-description=""
+                    data-item-image={product.Image}
+                    data-item-name={product.title}
+                  >
+                    Add to Cart
+                  </button>
+                </p>
+              </li>
+            ))}
           </ul>
         </Container>
       </main>
@@ -81,4 +86,37 @@ export default function Home() {
       />
     </div>
   );
+}
+
+export async function getStaticProps() {
+  const client = new ApolloClient({
+    uri: "https://e-commerce-nextjs.hasura.app/v1/graphql",
+    headers: {
+      "Content-Type": "application/json",
+      "x-hasura-admin-secret":
+        "18i97XDhSr4z3PX3hb0gwsJJNtZbPsZ81p1PowYUoo3adoy1UqCW152U3CMhQ8y9",
+    },
+    cache: new InMemoryCache(),
+  });
+
+  const response = await client.query({
+    query: gql`
+      query Product {
+        product {
+          id
+          image
+          price
+          title
+        }
+      }
+    `,
+  });
+
+  let productsdata = response.data.product;
+
+  return {
+    props: {
+      productsdata,
+    },
+  };
 }
